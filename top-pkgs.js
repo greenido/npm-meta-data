fca63884f4c60af4eb384cf633202ae0f35aa2f5
Ido Green <greenido@gmail.com>
2019-06-07 15:18:44 -0700

    Update top-pkgs.js

--- top-pkgs.js
+++ top-pkgs.js
@@ -5,12 +5,21 @@
  *  Update: June 2019
  *  
  * See: 
+ * 
    Use: view-source: https://www.npmjs.com/browse/depended?offset=36
  * https://www.npmjs.com/package/libnpmsearch
  * 
- * Result
- * ["lodash","request","chalk","react","express","commander","moment","debug","async","prop-types","react-dom","bluebird","fs-extra","tslib","axios","underscore","uuid","mkdirp","classnames","vue","body-parser","glob","yargs","webpack","rxjs","colors","babel-runtime","inquirer","jquery","minimist","babel-core","aws-sdk","yeoman-generator","core-js","through2","dotenv","redux","babel-loader","semver","winston","q","cheerio","typescript","eslint","rimraf","css-loader","@types/node","@angular/core","react-redux","shelljs","style-loader","js-yaml","@angular/common","zone.js","node-fetch","babel-eslint","ramda","gulp","object-assign","file-loader","babel-polyfill","@angular/platform-browser","request-promise","babel-preset-es2015","mongoose","@angular/compiler","gulp-util","handlebars","eslint-plugin-import","@babel/runtime","mocha","mongodb","lodash","request","chalk","react","express","commander","moment","debug","async","prop-types","react-dom","bluebird","fs-extra","tslib","axios","underscore","uuid","mkdirp","classnames","vue","body-parser","glob","yargs","webpack","rxjs","colors","babel-runtime","inquirer","jquery","minimist","babel-core","aws-sdk","yeoman-generator","core-js","through2","dotenv","redux","babel-loader","semver","winston","q","cheerio","typescript","eslint","rimraf","css-loader","@types/node","@angular/core","react-redux","shelljs","style-loader","js-yaml","@angular/common","zone.js","node-fetch","babel-eslint","ramda","gulp","object-assign","file-loader","babel-polyfill","@angular/platform-browser","request-promise","babel-preset-es2015","mongoose","@angular/compiler","gulp-util","handlebars","eslint-plugin-import","@babel/runtime","mocha","mongodb","@angular/forms","eslint-plugin-react","@angular/platform-browser-dynamic","superagent","url-loader","@angular/http","jsonwebtoken","webpack-dev-server","joi","@angular/router","node-sass","socket.io","redis","html-webpack-plugin","ora","immutable","postcss-loader","chai","ws","yosay","ejs","ember-cli-babel","autoprefixer","xml2js","bootstrap","coffee-script","morgan","styled-components","path","@babel/core","react-router-dom","cookie-parser","reflect-metadata","jest","sass-loader"]
+ * Result:
+ * ["lodash","request","chalk","react","express","commander","moment","debug","async","prop-types","react-dom","bluebird","fs-extra","tslib","axios","underscore",
+ * "uuid","mkdirp","classnames","vue","body-parser","glob","yargs","webpack","rxjs","colors","babel-runtime","inquirer","jquery","minimist","babel-core","aws-sdk",
+ * "yeoman-generator","core-js","through2","dotenv","redux","babel-loader","semver","winston","q","cheerio","typescript","eslint","rimraf","css-loader","@types/node",
+ * "@angular/core","react-redux","shelljs","style-loader","js-yaml","@angular/common","zone.js","node-fetch","babel-eslint","ramda","gulp","object-assign","file-loader",
+ * "babel-polyfill","@angular/platform-browser","request-promise","babel-preset-es2015","mongoose","@angular/compiler","gulp-util","handlebars","eslint-plugin-import",
+ * "@babel/runtime","mocha","mongodb","lodash","request","chalk","react","express","commander","moment","debug","async","prop-types","react-dom","bluebird","fs-extra","tslib","axios","underscore","uuid","mkdirp","classnames","vue","body-parser","glob","yargs","webpack","rxjs","colors","babel-runtime","inquirer","jquery","minimist","babel-core","aws-sdk","yeoman-generator","core-js","through2","dotenv","redux","babel-loader","semver","winston","q","cheerio","typescript","eslint","rimraf","css-loader","@types/node","@angular/core","react-redux","shelljs","style-loader","js-yaml","@angular/common","zone.js","node-fetch","babel-eslint","ramda","gulp","object-assign","file-loader","babel-polyfill","@angular/platform-browser","request-promise","babel-preset-es2015","mongoose","@angular/compiler","gulp-util","handlebars","eslint-plugin-import","@babel/runtime","mocha","mongodb","@angular/forms","eslint-plugin-react","@angular/platform-browser-dynamic","superagent","url-loader","@angular/http","jsonwebtoken","webpack-dev-server","joi","@angular/router","node-sass","socket.io","redis","html-webpack-plugin","ora","immutable","postcss-loader","chai","ws","yosay","ejs","ember-cli-babel","autoprefixer","xml2js","bootstrap","coffee-script","morgan","styled-components","path","@babel/core","react-router-dom","cookie-parser","reflect-metadata","jest","sass-loader"]
  * 
+ * 
+ *  TODO:
+ *  check the list of pkg from: https://gist.github.com/anvaka/8e8fa57c7ee1350e3491
  */
 const got = require('got');
 const fs = require('fs');
