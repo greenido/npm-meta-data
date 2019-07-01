ca0fda12a46e9852714daccd62aae4368632ddfb
Ido Green <greenido@gmail.com>
2019-07-01 12:37:34 +0300

    Update meta-data.sql

--- meta-data.sql
+++ meta-data.sql
@@ -1,5 +1,5 @@
 #
-#
+# Table to hold the packages and their 'tags' / keywords (e.g. authentication, networking etc')
 #
 CREATE TABLE pkgs (
     pkgId int NOT NULL AUTO_INCREMENT,
@@ -14,7 +14,7 @@ CREATE TABLE pkgs (
 );
 
 #
-#
+# Table to hold the keywords/tags for all pkgs. and their pop.
 #
 CREATE TABLE keywords (
     tagName varchar(255) NOT NULL,
@@ -23,6 +23,9 @@ CREATE TABLE keywords (
     PRIMARY KEY (tagName)
 );
 
+#
+# A quick way to create the keywords table only with SQL
+#
 INSERT INTO keywords (tagName, comments) 
 SELECT SUBSTRING_INDEX(keywords, ',', 1), comments FROM pkgs
 ON DUPLICATE KEY UPDATE howMany = howMany + 1;
